3752cca6b97af0b30f00b8e45ea7cbfb8a2872c5
Yaniv cohen <yanivomc@gmail.com>
2013-07-12 23:55:32 +0300

    Added the new HOME Page and CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 MyMaidAlpa::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
