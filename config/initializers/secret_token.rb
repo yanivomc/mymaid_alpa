f2047f74275502f17d5c553dd706c0a066a1e5f9
Yaniv cohen <yanivomc@gmail.com>
2013-07-12 23:26:40 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyMaidAlpa::Application.config.secret_token = '997854481d1c806bfea97b2dc5044699a848f85e4b465c5854b403d6a2a4d9d4339660bb942ab9df55c2348c408e3c93baf48bd58cd599ae83b401d278539a16'
