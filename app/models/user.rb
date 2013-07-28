a9649abd89e7aefe010ef72c2bd2041dabf64519
Yaniv cohen <yanivomc@gmail.com>
2013-07-28 23:22:42 +0300

    FACEBOOK Fixed First name

--- app/models/user.rb
+++ app/models/user.rb
@@ -93,7 +93,7 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
-      user.first_name = auth.info.first
+      user.first_name = auth.info.first_name
       user.last_name  = auth.info.last_name
       user.email = auth.info.email
       user.image_url  = auth.info.image
