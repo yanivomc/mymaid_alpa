85d04004e87abc5e0daf022c7b60a2fb20195d64
Yaniv Cohen <yanivomc@gmail.com>
2013-08-05 11:10:58 +0300

    Removed HTTPS Support

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module MyMaidAlpa
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-    config.middleware.insert_before ActionDispatch::Static, Rack::SSL, :exclude => proc { |env| env['HTTPS'] != 'on' }
+
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
